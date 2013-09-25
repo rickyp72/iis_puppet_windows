9202aed08c0ff1a6d3782e51cf9679fa95b4a6da
ricky <ricky@192.168.101.147>
2013-09-25 15:17:21 +0100

    removed require on filesrepo.pp

--- modules/windows-iis/manifests/filesrepo.pp
+++ modules/windows-iis/manifests/filesrepo.pp
@@ -3,19 +3,19 @@ class windows-iis::filesrepo {
     exec { "DotNet35Inst":
     command => "O:/IIS_Packages/dotnet35.exe /q",
     refreshonly => true,
-    require => File["O:/IIS_Packages/dotnet35.exe"],
+
   }
   
     exec { "DotNet40Inst":
     command => "O:/IIS_Packages/dotnet40.exe /q",
     refreshonly => true,
-    require => File["O:/IIS_Packages/dotnet40.exe"],
+    
   }
   
     exec { "DotNet45Inst":
     command => "O:/IIS_Packages/dotnet45.exe /q",
     refreshonly => true,
-    require => File["O:/IIS_Packages/dotnet45.exe"],
+   
   }
   
 }
