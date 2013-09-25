07a7c99a2ed1265b0eba70fb8989b7419d067c3a
ricky <ricky@192.168.101.147>
2013-09-25 15:26:21 +0100

    change install file path

--- modules/aspnetmvc4/manifests/install.pp
+++ modules/aspnetmvc4/manifests/install.pp
@@ -1,16 +1,18 @@
 class aspnetmvc4::install {
-      file { 'C:/windows/temp/AspNetMVC4Setup.exe':
-    source => 'puppet:///modules/aspnetmvc4/AspNetMVC4Setup.exe',
-    owner => 'administrator',
-    group => 'administrators',
-    mode => 0770,
-    notify => Exec["ASPNETMVC4"],
-  }
+#      file { 'C:/windows/temp/AspNetMVC4Setup.exe':
+#    source => 'puppet:///modules/aspnetmvc4/AspNetMVC4Setup.exe',
+#    owner => 'administrator',
+#    group => 'administrators',
+#    mode => 0770,
+#    notify => Exec["ASPNETMVC4"],
+#  }
   
+ # this will fail in dev as it is getting package from repo
+ # setup mapping to  
     exec { "ASPNETMVC4":
-    command => "C:/windows/temp/AspNetMVC4Setup.exe /q",
+    command => "O:/IIS_Packages/AspNetMVC4Setup.exe /q",
     refreshonly => true,
-    require => File["C:/windows/temp/AspNetMVC4Setup.exe"],
+    #require => File["C:/windows/temp/AspNetMVC4Setup.exe"],
     notify => Class["aspnetmvc4::service"],
   }
 }
