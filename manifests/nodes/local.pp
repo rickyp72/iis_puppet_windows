16e7ab6bf010248adb94ccde61d13e7219ce59a8
ricky <ricky@172.16.166.1>
2013-09-25 18:30:26 +0100

    added rackspace

--- manifests/nodes/local.pp
+++ manifests/nodes/local.pp
@@ -5,61 +5,6 @@
 
 
 
-########################################################
-#####        WSpace Windows IIS servers             ####
-########################################################
-
-#node 'WGB01WA6005.bbc.worldwide.co.uk' inherits systestnode {
-#include windows-iis
-#include aspnetmvc4
-#  
-#}
-
-node 'WGB01WA7020.worldwide.bbc.co.uk' inherits uatnode {
-include windows-iis
-include aspnetmvc4
-
-  
-}
-
-node 'WGB01WA7019.worldwide.bbc.co.uk' inherits uatnode {
-include windows-iis
-include aspnetmvc4
-
-#      iis_apppool {'Gateway':
-#        ensure                => present,
-#        managedpipelinemode   => 'Integrated',
-#        managedruntimeversion => 'v2.0',
-#      }
-#
-#      iis_site {'Gateway':
-#        ensure          => present,
-#        bindings        => ["http/*:8000:"],
-#      }
-#
-#      iis_site {'Gateway':
-#        ensure          => present,
-#        bindings        => ["http/*:80:"],
-#      }
-#
-#      iis_site {'Gateway':
-#        ensure          => present,
-#        bindings        => ["http/*wspace-uat:80:"],
-#      }
-#
-#      iis_app {'Gateway/':
-#        ensure          => present,
-#        applicationpool => 'Gateway',
-#      }
-#
-#      iis_vdir {'Gateway/':
-#        ensure          => present,
-#        iis_app         => 'Gateway/',
-#        physicalpath    => 'c:inetpub\wwwroot\gateway'
-#      }
-
-  
-}
 
 ########################################################
 #####        Windows test IIS servers               ####
